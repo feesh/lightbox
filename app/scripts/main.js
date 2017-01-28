914b78862314caa2f5ac029fce90059cce2486ac
Trish Ang <trish@counsyl.com>
2017-01-28 13:48:49 -0800

    Test example using Flickr API

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+// Main JS for Lightbox gallery
+
+// Test API call
+function callIG() {
+  var api_key = 'e0bc054b3d94fd9c1f91199ef47ae646';
+
+  var url = 'https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key=' + api_key + '&photoset_id=72157640077865876&format=json&nojsoncallback=1';
+
+  var request = new XMLHttpRequest();
+  request.open('GET', url, true);
+
+  console.log('allo allo');
+
+  request.onload = function() {
+    if (this.status >= 200 && this.status < 400) {
+      // Success!
+      var data = this.response;
+      console.log(data);
+    } else {
+      // We reached our target server, but it returned an error
+      console.log('error: ');
+      console.log(data);
+    }
+  }
+
+  request.onerror = function() {
+    // There was a connection error of some sort
+  };
+
+  request.send();
+
+};
+
+callIG();
