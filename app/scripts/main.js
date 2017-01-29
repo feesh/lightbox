f288a64fc7f3fb83f49accc2b232e82bec2452a5
Trish Ang <trish@counsyl.com>
2017-01-29 10:49:35 -0800

    Adding utilities for Flickr URLs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,18 +16,7 @@ function callFlickr() {
       // Success!
       var data = JSON.parse(this.response);
       console.log(data);
-
-      // Test spit results out to page
-      // function displayImages(data) {
-        var testdiv = document.getElementById('test');
-        for (var i = 0; i < data.photoset.photo.length; i++) {
-          var currentPhoto = data.photoset.photo[i];
-          // Format URL for image src
-          // https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-          var imgURL = 'https://farm' + currentPhoto.farm + '.staticflickr.com/' + currentPhoto.server +
-      '/' + currentPhoto.id + '_' + currentPhoto.secret + '_q.jpg';
-          testdiv.innerHTML = testdiv.innerHTML + '<p><img src="' + imgURL + '"/></p>';
-        }
+      showThumbnails(data);
     } else {
       // We reached our target server, but it returned an error
       console.log('error: ');
@@ -40,6 +29,38 @@ function callFlickr() {
 
   request.send();
 };
+
+// Format thumbnails
+function showThumbnails(data) {
+  var thumbnails = document.getElementById('thumbnails');
+
+  // For each photo, generate a thumbnail and show on page
+  for (var i = 0; i < data.photoset.photo.length; i++) {
+    var currentPhoto = data.photoset.photo[i];
+    var thumbURL = buildThumbnailURL(currentPhoto);
+
+    // Append to a gallery container div
+    thumbnails.innerHTML = thumbnails.innerHTML + '<li><img src="' + thumbURL + '"/></li>';
+  }
+};
+
+
+// Utilities for working with Flickr image URLs
+// Docs: https://www.flickr.com/services/api/misc.urls.html
+function buildThumbnailURL(photo) {
+  // Thumbnail format: https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_q.jpg
+  return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_q.jpg';
+}
+
+function buildPhotoURL(photo) {
+  // Photo format: https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+  return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
+}
+
+function buildLargePhotoURL(photo) {
+  // Photo format: https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}_b.jpg
+  return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_b.jpg';
+}
 
 
 // Function calls
