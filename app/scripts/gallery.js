7ed1ef108673410f352817507c055635afad7987
Trish Ang <trish@counsyl.com>
2017-01-29 11:43:27 -0800

    Testing set up to show current image

--- app/scripts/gallery.js
+++ app/scripts/gallery.js
@@ -3,8 +3,37 @@
 (function(document, window) {
   'use strict';
 
+  // Set up gallery with ref to photos and DOM
+  function Gallery(photos) {
+    this.currentIndex = 0;
+    this.photos = photos;
+
+    // Initialize by showing the first photo
+    this.showPhoto(this.currentIndex);
+  }
+
+  // Show the current photo
+  Gallery.prototype.showPhoto = function(index) {
+    if (index >= 0 && index < this.photos.length) {
+      this.currentIndex = index;
+      console.log(this.currentIndex);
+      console.log(this.photos[this.currentIndex]);
+
+      var container = document.getElementById('currentphoto');
+      var currentPhoto = this.photos[this.currentIndex];
+      var imgURL = Flickr.buildLargePhotoURL(currentPhoto);
+
+      // Display current image in display area
+      container.innerHTML = container.innerHTML + '<img src="' + imgURL + '"/>';
+    }
+  }
+
+  // Show the previous photo
+
+  // Show the next photo
+
   // Format thumbnails
-  function showThumbnails(data) {
+  Gallery.prototype.showThumbnails = function(data) {
     var thumbnails = document.getElementById('thumbnails');
 
     // For each photo, generate a thumbnail and show on page
@@ -17,7 +46,5 @@
     }
   };
 
-  window.Gallery = {
-    showThumbnails: showThumbnails
-  }
+  window.Gallery = Gallery;
 })(document, window);
