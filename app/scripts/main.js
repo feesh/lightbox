1f94531f931db49f7c1ef7ff79138ca4020c9686
Trish Ang <trish@counsyl.com>
2017-01-29 21:12:34 -0800

    Adding keydown checker for navigation.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,29 +24,54 @@
     var closeBtn = document.getElementById('toggleclose');
     var overlay = document.getElementById('overlay');
 
+    // Activate previous button
     prevBtn.addEventListener('click', function(event) {
       event.preventDefault();
       console.log('toggle prev');
       gallery.showPrevious.bind(gallery)();
     });
 
+    // Activate next button
     nextBtn.addEventListener('click', function(event) {
       event.preventDefault();
       console.log('toggle next');
       gallery.showNext.bind(gallery)();
     });
 
+    // Activate close button
     closeBtn.addEventListener('click', function(event) {
       event.preventDefault();
       console.log('toggle close');
       gallery.closeLightbox();
     });
 
+    // Close lightbox when clicking on the overlay also
     overlay.addEventListener('click', function(event) {
       event.preventDefault();
       console.log('toggle close');
       gallery.closeLightbox();
     });
+
+    // Set up keydown checks
+    document.onkeydown = checkKey;
+
+    function checkKey(e) {
+      e = e || window.event;
+
+      if (e.keyCode == '37') {
+        // left arrow
+        console.log('toggle prev with key');
+        gallery.showPrevious.bind(gallery)();
+      } else if (e.keyCode == '39') {
+        // right arrow
+        console.log('toggle next with key');
+        gallery.showNext.bind(gallery)();
+      } else if (e.keyCode == '27') {
+        // esc key
+        console.log('exit lightbox with key');
+        gallery.closeLightbox();
+      }
+    }
   }
 
   window.Main = {
